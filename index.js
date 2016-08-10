196ee6569d6003925fe1e1fc67da306d7b5f09ac
Hudson Prestidge <charcon@ihug.co.nz>
2016-08-10 15:00:40 +1200

    bot can join twitch channels and respond to messages

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 var irc = require('irc');
+var passwords = require('./password')
+
 var client = new irc.Client('irc.chat.twitch.tv', 'LucidityBot', {
-  channels: ['#Charcon'],
+  channels: ['#charcon'],
+  password: passwords.twitchPassword,
 });
-//
-// client.addListener('message', function (from, to, message) {
-//   client.say('', "I'm a bot!")
-// });
-//
+
+client.join("#charcon", function (res, err){
+  client.say("#charcon", "Hello there, I'm LucidityBot!")
+})
+
+
+client.addListener('message', function (from, to, message) {
+    client.say("#charcon", "I am hearing you talk!")
+});
+
 // client.addListener('pm', function (from, message) {
 //   client.say('Charcon', "SRSLY, I AM A BOT!");
 // });
