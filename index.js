1f83312432c8a2820aba9dafc617c3ddb77f9963
Hudson Prestidge <charcon@ihug.co.nz>
2016-08-14 15:48:50 +1200

    bot responds to commands from the database with appropriate response

--- index.js
+++ index.js
@@ -9,18 +9,19 @@ knex('commands')
   })
 
 var client = new irc.Client('irc.chat.twitch.tv', 'LucidityBot', {
-  channels: ['#charcon'],
+  channels: ['#Charcon'],
   password: passwords.twitchPassword,
 });
 
-client.join("#charcon", function (res, err){
-  client.say("#charcon", "Hello there, I'm LucidityBot!")
+client.join("#Charcon", function (res, err){
+  client.say("#Charcon", "Hello there, I'm LucidityBot!")
 })
 
 client.addListener('message', function (from, to, message) {
-    // client.say("#charcon", from + ", I am hearing you talk!")
+  checkForCommand(message)
+    // client.say("#Charcon", from + ", I am hearing you talk!")
     // if(checkForCommand(message)){
-    //   client.say("#charcon", "you are calling the " + checkForCommand(message) + " command!"  )
+    //   client.say("#Charcon", "you are calling the " + checkForCommand(message) + " command!"  )
     // }
 });
 
@@ -31,3 +32,17 @@ client.addListener('error', function(message) {
  //essentially what we want is check the text you've just seen for a command
   // this means go through the commands, and check if the substring at the start
   // of the message matches that command. If it does, the message contains that command.
+
+  function checkForCommand(message) {
+    // var firstword = message.split(' ')[0]
+    var match = message.match(/!(\w+)/)
+    if (match){
+      firstWord = match[1]
+      knex('commands')
+      .where('name', firstWord)
+      .first()
+      .then(function(command){
+        client.say("#Charcon", command.response)
+      })
+    }
+  }
--- seeds/commands_seed.js
+++ seeds/commands_seed.js
@@ -5,8 +5,8 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       return Promise.all([
         // Inserts seed entries
-        knex('commands').insert({id: 1, name: '!lucitest', response: 'wow, excellent testing!'}),
-        knex('commands').insert({id: 2, name: '!songreq', response: 'song request day is on thursday, thanks for asking!'})
+        knex('commands').insert({id: 1, name: 'lucitest', response: 'wow, excellent testing!'}),
+        knex('commands').insert({id: 2, name: 'songreq', response: 'song request day is on thursday, thanks for asking!'})
       ]);
     });
 };
