d72ab46613418f83d7b16e95df929fd553b5b553
Hudson Prestidge <charcon@ihug.co.nz>
2016-10-24 17:10:38 +1300

    clean up scheduled commands code, store intervals so they can be cleared later

--- index.js
+++ index.js
@@ -32,27 +32,12 @@ function init(){
 }
 
 function setScheduledCommands() {
+  var scheduled_commands = []
   knex('scheduled_commands')
     .innerJoin('commands', 'scheduled_commands.command_id', 'commands.id')
     .then(function(commands){
-      commands.map(function (command){
-
-        setInterval( function(){runCommand(command)}, command.frequency*1000 )
-
-        // let frequency = command.frequency
-        // let hours = parseInt(frequency / 3600)
-        // let minutes = parseInt((frequency % 3600) / 60)
-        // let seconds = parseInt(frequency % 60)
-        // let cronString = ""
-        // seconds ? cronString += '*/' + seconds + " " : cronString += "0 "
-        // minutes ? cronString += '*/' + minutes + " " : cronString += "* "
-        // hours ? cronString += '*/' + hours + " " : cronString += "* "
-        // cronString += "* * *"
-        // console.log(cronString);
-        // cron.schedule(cronString, function() {
-        //   runCommand(command)
-        // })
-        // return command
+      scheduled_commands = commands.map(function (command){
+        return setInterval( function(){runCommand(command)}, command.frequency*1000 )
       })
     })
 }
